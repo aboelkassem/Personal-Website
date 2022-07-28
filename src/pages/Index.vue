b69decab23440dd718ea68cf071b0d7026fa58e4
Mohamed Abdelrahman <mohamedabdelrahman972@gmail.com>
2022-07-29 01:09:55 +0200

    Fix UI

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -123,7 +123,7 @@
                             height="20px"
 							src="/avatar.jpg"
 							alt="avatar"
-							class="w-72 min-h-full mb-8 lg:mb-0 rounded-lg"
+							class="w-64 h-full mb-8 lg:mb-0 rounded-lg"
 						/>
 					</div>
 					<div class="bg-background-tertiary flex-1 text-lg sm:text-xl ml-6">
